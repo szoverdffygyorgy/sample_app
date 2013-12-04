a329c7bd681c4e54122ef6d95f4f88e96e1e9c9a
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2013-12-04 16:11:13 +0100

    Finish user signup

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,9 @@
-require 'spec_helper'
+module UsersHelper
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
-describe UsersHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
